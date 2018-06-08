fc81bbc12ab3fff2c169c11ecee57e7b523e2448
water-law <2645422611@qq.com>
2018-06-08 10:52:38 +0800

    alter bat

--- trans.bat
+++ trans.bat
@@ -7,6 +7,5 @@ call "C:\Program Files\QGIS 3.0\bin\py3_env.bat"
 ::pyrcc5 -o resources.py resources.qrc && pyuic5 -o Ui_Zoomer.py Ui_Zoomer.ui
 @call:trans
 :trans
-@FOR %%F IN (*.qrc) DO pyrcc5 -o %%~nF.py %%F
-@FOR %%F IN (*.ui) DO pyuic5 -o %%~nF.py %%F
+@FOR %%F IN (*.qrc) DO pyrcc5 -o %%~nF.py %%F && @FOR %%F IN (*.ui) DO pyuic5 -o %%~nF.py %%F
 goto:eof
